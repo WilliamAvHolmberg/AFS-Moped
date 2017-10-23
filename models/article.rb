76b71885d2f4cda8fdb3330e26eef0fa354ce009
itggot-william-holmberg <30773487+WilliamAvHolmberg@users.noreply.github.com>
2017-10-23 22:29:33 +0200

    Fixed typo in Article

--- models/article.rb
+++ models/article.rb
@@ -2,7 +2,7 @@ class Article
   include DataMapper::Resource
 
   property :id,                               Serial    # An auto-increment integer key
-  property :article_number, :unique => true   String    # A unique 'number' to keep track of our articles
+  property :article_number,                   String    # A unique 'number' to keep track of our articles
   property :suppliers_article_number,         String    # The article number that our supplier use for the specific article
   property :name,                             String    # Name of the Article
   property :created_at,                       DateTime  # A DateTime, for any date you might like.
